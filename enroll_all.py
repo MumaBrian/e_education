2c24de88e1ff79004062f7e9e4c79287cf3fca60
MumaBrian <brianmuma5@gmail.com>
2022-07-08 04:55:01 +0100

    api

---
+++ enroll_all.py
@@ -0,0 +1,25 @@
+import requests
+
+username = 'muma'
+password = 'user1234'
+
+base_url = 'http://127.0.0.1:8000/api/'
+
+# retrieve all courses
+r = requests.get(f'{base_url}courses/')
+courses = r.json()
+
+available_courses = ', '.join([course['title'] for course in courses])
+print(f'Available courses: {available_courses}')
+
+
+
+
+for course in courses:
+    course_id = course['id']
+    course_title = course['title']
+    r = requests.post(f'{base_url}courses/{course_id}/enroll/',
+                      auth=(username, password))
+    if r.status_code == 200:
+      # successful request
+      print(f'Successfully enrolled in {course_title}')
